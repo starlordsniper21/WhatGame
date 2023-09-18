04780a5eea8358ea53aafbc06cee609a7616ca57
starlordsniper21 <michaelyllor@gmail.com>
2023-09-18 18:14:01 +0800

    Minor Changes

--- Assets/Scripts/Core/CameraFollow.cs
+++ Assets/Scripts/Core/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     private Vector3 offset = new Vector3(0f, 0f, -10f);
-    private float smoothTime = 0.25f;
+    private float smoothTime = 0.15f;
     private Vector3 velocity = Vector3.zero;
 
     [SerializeField] private Transform target;
